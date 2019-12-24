0de89545e81cfbf14cc6e0a635b3c98147e76f19
LilDaddy <bezhuraedgar@gmail.com>
2019-12-24 09:31:41 +0200

    Init

---
+++ venv/include/app.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import os
+import sys
+import webbrowser
+
+def talk(words):
+    print(words)
+    os.system("say " + words)
+
+talk("Hi, ask me some things")
+
+def command():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Speak")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        zadanie = r.recognize_google(audio).lower()
+        print("You say: " + str(command))
+
+    except sr.UnknownValueError:
+        talk("I don't understand you")
+        zadanie = command()
+
+    return zadanie
+
+def makeSomething(zadanie):
+    if 'super' in zadanie:
+        talk("Already opening")
+        url = 'https://google.com'
+        webbrowser.open(url)
+    elif 'stop' in zadanie:
+        talk('Yes, of course')
+        sys.exit()
+    elif 'name' in zadanie:
+        talk("My name is Siri")
+
+while True:
+    makeSomething(command())
