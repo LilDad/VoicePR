8dc4c62be54392c32a89291edf173b390e37556e
LilDaddy <bezhuraedgar@gmail.com>
2020-01-03 20:34:17 +0200

    def Talk

--- venv/include/app.py
+++ venv/include/app.py
@@ -2,3 +2,11 @@ import speech_recognition as sr
 import os
 import sys
 import webbrowser
+
+# Функция воспроизведения текста
+def talk(words):
+    print(words)
+    os.system("Say " + words)
+
+talk("Hi. Ask me something.")
+
