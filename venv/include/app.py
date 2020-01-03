3d7eb85ed1e2979cb07487ce462a4aab4ab425b8
LilDaddy <bezhuraedgar@gmail.com>
2020-01-03 20:42:36 +0200

    def Command

--- venv/include/app.py
+++ venv/include/app.py
@@ -10,3 +10,24 @@ def talk(words):
 
 talk("Hi. Ask me something.")
 
+# Функция для прослушки
+def command():
+    r = sr.Recognizer()
+
+    # Активация микрофона с паузой
+    with sr.Microphone() as source:
+        print("Speak")
+        r.pause_threshold = 1
+
+        # Отсекание фона
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        zadanie = r.recognize_google(audio).lower()
+        print("You say: " + zadanie)
+    # Исключение ошибки
+    except sr.UnknownValueError:
+        talk("I don't understand you.")
+
+    return zadanie
